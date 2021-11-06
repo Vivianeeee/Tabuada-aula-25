f3a23d91ffdc4afa8dc41675ab8c8165117257ec
vivianeeee <vivianeoliveirandrade@gmail.com>
2021-11-06 04:07:16 -0300

    Exercicio tabuada

---
+++ tabuadaaula25.js
@@ -0,0 +1,75 @@
+let contador = 1;
+
+while (contador <= 10) {
+    console.log(`Valor = ${contador}`);
+    //contador++; // contador = contador + 1
+  resultado=(12)
+  3 *  4 ('tabuada') ('i++ 12')
+
+    contador = contador + 2;
+    contador2 = contador2 - 1;
+}
+
+console.clear();
+
+//WHILE ESTRUTURA DE REPETIÇÃO
+let contador2 = 10;
+
+while (contador2 >= 0) {
+    console.log(`Valor = ${contador2}`);
+
+    resultado=(24)
+    3 *  8 ('tabuada') ('i++ 24')
+  
+    contador = contador + 2;
+    contador2 = contador2 - 1;
+}
+
+console.clear();
+// DO-WHILE ESTRUTURA DE REPETIÇÃO
+let i = 0;
+text = '';
+
+do {
+    text += `O número é ${i}\n`; 
+
+    resultado=(18)
+    3 *  6 ('tabuada') ('i++ 18')
+    contador = contador + 2;
+    contador2 = contador2 - 1;
+     
+    i++;
+} while (i < 10);
+console.log(text);
+
+console.clear();
+// FOR LAÇO DE REPETIÇÃO
+for (let i = 0; i <= 10; i = i + 1) console.log(`Laço for número: ${i}`);
+
+resultado=(30)
+3 *  10 ('tabuada') ('i++ 30')
+contador2 = contador + 2;
+    contador2 = contador2 - 1;
+  
+
+console.clear();
+
+// LAÇOS DE REPETIÇÃO COM ARRAYS
+const frutas = [
+    'maçã',
+    'Laranja',
+    'melancia',
+    10,
+    true,
+    'Catarina',
+    'Irmão do jorel',
+];
+resultado=(9)
+3 *  3 ('tabuada')('i++ 9')
+contador = contador + 2;
+    contador2 = contador2 - 1;
+    
+console.log(frutas);
+
+for (let i = 0; i < frutas.length; i++)
+    console.log(`O nome da fruta é: ${frutas[i]}`);
